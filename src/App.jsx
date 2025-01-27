9694390fab128691590daeeff8e96227cf47461e
Mitzi Vite <vit20001@byui.edu>
2025-01-27 16:46:25 -0600

    login interface phone

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,72 @@
 import './App.css';
-
+import UserIcon from './assets/images/user.png';
+import PasswordIcon from './assets/images/password.png';
+import FacebookIcon from './assets/images/facebook.png';
+import GoogleIcon from './assets/images/google.png';
+import CloudIcon from './assets/images/cloud.png';
+import FlowerIcon from './assets/images/flower.png';
+import DotIcon from './assets/images/fill-dot-background.png';
+import EmptyCircle from './assets/images/empty-circle.png';
 function App() {
   return (
     <div className="login-page">
+      <img className='clouds' src={CloudIcon} alt="cloud-icon" />
+      <img className='flowers' src={FlowerIcon} alt="flower-icon" />
+      <img className='dots' src={DotIcon} alt="dot-icon" />
+      <img className='dots-two' src={DotIcon} alt="dot-icon" />
+      <img className='dots-three' src={DotIcon} alt="dot-icon" />
+      <img className='dots-four' src={DotIcon} alt="dot-icon" />
+      <img className='dots-five' src={DotIcon} alt="dot-icon" />
+      <img className='empty-circle' src={EmptyCircle} alt="empty-circle" />
+      <div className="main-container-login">
+      <div className='welcome-container'>
       <p>WELCOME TO</p>
-      
+      </div>
 
       <div className="signIn-main-container">
         <div className="signIn-container">
         <h1>WARDROBY</h1>
         <form>
-          <div className='user-input-container'>
-            <svg
-              className='user-icon'
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-              fill="white"
-            >
-            <circle cx="12" cy="8" r="4" />
-            <path d="M12 14c-5 0-9 2.5-9 5.5V22h18v-2.5c0-3-4-5.5-9-5.5z" />
-            </svg>
-            <input className='username' placeholder='Username' />
-          </div>
+          <div className="user-input-container">
+              <img src={UserIcon} alt="User Icon" className="user-icon" />
+              <input className="username" placeholder="Username" />
+            </div>
 
           <div className='password-input-container'>
-            <svg className='password-icon'
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-            fill="white"
-            >
-            <path d="M12 1C9.243 1 7 3.243 7 6v4H5c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2V12c0-1.103-.897-2-2-2h-2V6c0-2.757-2.243-5-5-5zm0 2c2.206 0 4 1.794 4 4v4H8V6c0-2.206 1.794-4 4-4zm-1 10h2v6h-2v-6z"/>
-            </svg>
-            <input className='password' placeholder='Password' />
-            </div>
-          <p><a>Forgot password?</a></p>
+          
+            <img src={PasswordIcon} alt='Password Icon' className='password-icon' />
+          <input className='password' placeholder='Password' />
+          </div>
+          <p className='forgot-password'><a>Forgot password?</a></p>
           <button className='signIn-button'>Sign In</button>
-          <p>Or sign in with</p>
-          <button className='google-button'>Google</button>
-          <button className='facebook-button'>Facebook</button>
-          <p>Don&apos;t have an account? <a href='#'>Sign Up</a></p>
+          <div className="or-sign-with-container">
+              <hr className="divider" />
+              <p className="or-sign-with">Or sign in with</p>
+              <hr className="divider" />
+            </div> 
+          <div className='social-media-icons'>
+          <div className='social-media-square'>
+          <img src={FacebookIcon} alt='Facebook Icon' className='facebook-icon' />
+          </div>
+          <div className='social-media-square'>
+          <img src={GoogleIcon} alt='Google Icon' className='google-icon' />
+          </div>
+          </div>
+          <p className='no-account'>Don&apos;t have an account? <a href='#'>Sign Up</a></p>
         </form>
         </div>
       </div>
+      </div>
+      <div className="flowers-bottom">
+        <img src={FlowerIcon} alt="Flower" style={{ width: '100%', height: '100%' }} />
+      </div>
+      <div className="vertical-line-one lines"></div>
+      <div className="vertical-line-two lines"></div>
+      <div className="vertical-line-three lines"></div>
+      <div className="vertical-line-four lines"></div>
+      <div className="vertical-line-five lines"></div>
+      <div className="vertical-line-six lines"></div>
+      <div className="vertical-line-seven lines"></div>
     </div>
   );
 }
