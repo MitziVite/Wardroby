67a028474c12a7d73bead1e736b862963b6da5f1
Mitzi Vite <vit20001@byui.edu>
2025-01-29 16:39:13 -0600

    keys changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import UserIcon from './assets/images/user.png';
 import PasswordIcon from './assets/images/password.png';
 import CloudIcon from './assets/images/cloud.png';
@@ -6,17 +7,19 @@ import FlowerIcon from './assets/images/flower.png';
 import DotIcon from './assets/images/fill-dot-background.png';
 import EmptyCircle from './assets/images/empty-circle.png';
 import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from "jwt-decode";
+import { jwtDecode } from 'jwt-decode';
+import Home from './Home';
 
 import { LoginSocialFacebook } from 'reactjs-social-login';
-import {  FacebookLoginButton } from "react-social-login-buttons";
+import { FacebookLoginButton } from 'react-social-login-buttons';
 
 import { useState } from 'react';
 
-function App() {
+function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
+  const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -32,6 +35,7 @@ function App() {
       if (response.ok) {
         setMessage(data.message);
         console.log(data.user);
+        navigate('/home');
       } else {
         setMessage(data.message);
       }
@@ -42,7 +46,6 @@ function App() {
   };
 
   return (
-    // const [profile, setProfile] = useState(null);
     <div className="login-page">
       <img className='clouds' src={CloudIcon} alt="cloud-icon" />
       <img className='flowers' src={FlowerIcon} alt="flower-icon" />
@@ -53,77 +56,73 @@ function App() {
       <img className='dots-five' src={DotIcon} alt="dot-icon" />
       <img className='empty-circle' src={EmptyCircle} alt="empty-circle" />
       <div className="main-container-login">
-      <div className='welcome-container'>
-      <p>WELCOME TO</p>
-      </div>
-
-      <div className="signIn-main-container">
-        <div className="signIn-container">
-        <h1>WARDROBY</h1>
-        <form onSubmit={handleLogin}>
-          <div className="user-input-container">
-              <img src={UserIcon} alt="User Icon" className="user-icon" />
-              <input
+        <div className='welcome-container'>
+          <p>WELCOME TO</p>
+        </div>
+        <div className="signIn-main-container">
+          <div className="signIn-container">
+            <h1 className='global-title'>WARDROBY</h1>
+            <form onSubmit={handleLogin}>
+              <div className="user-input-container">
+                <img src={UserIcon} alt="User Icon" className="user-icon" />
+                <input
                   className="username"
                   placeholder="Username"
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-
-          <div className='password-input-container'>
-            <img src={PasswordIcon} alt='Password Icon' className='password-icon' />
-            <input
+                />
+              </div>
+              <div className='password-input-container'>
+                <img src={PasswordIcon} alt='Password Icon' className='password-icon' />
+                <input
                   className='password'
                   placeholder='Password'
                   type='password'
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
+              </div>
+              <p className='forgot-password'><a href="#">Forgot password?</a></p>
+              <button className='signIn-button' type="submit">Sign In</button>
+              {message && <p>{message}</p>}
+              <div className="or-sign-with-container">
+                <hr className="divider" />
+                <p className="or-sign-with">Or sign in with</p>
+                <hr className="divider" />
+              </div>
+              <div className='social-media-icons'>
+                <div className='social-media-square'>
+                  <LoginSocialFacebook
+                    // appId="1185813582892730"
+                    appId={import.meta.env.VITE_FACEBOOK_APP_ID}
+                    onResolve={(response) => {
+                      console.log(response);
+                    }}
+                    onReject={(error) => {
+                      console.log(error);
+                    }}
+                  >
+                    <FacebookLoginButton />
+                  </LoginSocialFacebook>
+                </div>
+                <div className='social-media-square'>
+                  <GoogleLogin
+                    onSuccess={credentialResponse => {
+                      const credentialResponseDecoded = jwtDecode(
+                        credentialResponse.credential
+                      );
+                      console.log(credentialResponseDecoded);
+                    }}
+                    onError={() => {
+                      console.log('Login Failed');
+                    }}
+                  />
+                </div>
+              </div>
+              <p className='no-account'>Don&apos;t have an account? <a href='#'>Sign Up</a></p>
+            </form>
           </div>
-          <p className='forgot-password'><a href="#">Forgot password?</a></p>
-          <button className='signIn-button'>Sign In</button>
-          {message && <p>{message}</p>}
-          <div className="or-sign-with-container">
-              <hr className="divider" />
-              <p className="or-sign-with">Or sign in with</p>
-              <hr className="divider" />
-            </div> 
-          <div className='social-media-icons'>
-          <div className='social-media-square'>
-
-          <LoginSocialFacebook
-            appId={import.meta.env.VITE_FACEBOOK_APP_ID}
-            onResolve={(response) => {
-              console.log(response);
-            }}
-            onReject={(error) => {
-              console.log(error);
-            }}
-            >
-            <FacebookLoginButton/>
-          </LoginSocialFacebook>
-
-          </div>
-          <div className='social-media-square'>
-    
-          <GoogleLogin
-            onSuccess={credentialResponse => {
-              const credentialResponseDecoded = jwtDecode(
-                credentialResponse.credential
-                );
-              console.log(credentialResponseDecoded);
-            }}
-            onError={() => {
-              console.log('Login Failed');
-            }}
-            />
-          </div>
-          </div>
-          <p className='no-account'>Don&apos;t have an account? <a href='#'>Sign Up</a></p>
-        </form>
         </div>
-      </div>
       </div>
       <div className="flowers-bottom">
         <img src={FlowerIcon} alt="Flower" style={{ width: '100%', height: '100%' }} />
@@ -136,6 +135,19 @@ function App() {
       <div className="vertical-line-six lines"></div>
       <div className="vertical-line-seven lines"></div>
     </div>
+  );
+}
+
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+import './Home.css'; // Importar el archivo CSS
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1 className='global-title'>WARDROBY</h1>
+    </div>
+  );
+}
+
+export default Home;
